2c0e0988b1cda08b7230a71da30847d9b52f5faf
Caner Demir <96390657+piupiunn@users.noreply.github.com>
2024-09-30 17:49:11 +0300

    O5PP-550

--- Desktop/flexnet-frontend/src/components/Faq.js
+++ Desktop/flexnet-frontend/src/components/Faq.js
@@ -8,10 +8,10 @@ export default function Faq() {
     <>
       <div className="faq-parent container mx-auto px-6 py-10">
         <div className="faq-container">
-          <h1>About XYZ</h1>
+          <h1>General FlexNet Overview</h1>
           <div className="card accordion-box ">
             <Accordion activeIndex={0}>
-              <AccordionTab header="Header I">
+              <AccordionTab header="What is FlexNet and how does it work?">
                 <p className="m-0">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -23,7 +23,7 @@ export default function Faq() {
                   mollit anim id est laborum.
                 </p>
               </AccordionTab>
-              <AccordionTab header="Header II">
+              <AccordionTab header="Who can benefit from FlexNet solutions?">
                 <p className="m-0">
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem aperiam, eaque
@@ -35,7 +35,55 @@ export default function Faq() {
                   eius modi.
                 </p>
               </AccordionTab>
-              <AccordionTab header="Header III">
+              <AccordionTab header="What industries does FlexNet cater to?">
+                <p className="m-0">
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus.
+                </p>
+              </AccordionTab>
+              <AccordionTab header="What makes FlexNet different from other 4G/5G connectivity providers">
+                <p className="m-0">
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus.
+                </p>
+              </AccordionTab>
+              <AccordionTab header="Can I use FlexNet for both 4G and 5G networks?">
+                <p className="m-0">
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus.
+                </p>
+              </AccordionTab>
+              <AccordionTab header="What deployment models does FlexNet offer">
+                <p className="m-0">
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus.
+                </p>
+              </AccordionTab>
+              <AccordionTab header="Is FlexNet suitable for small businesses?">
                 <p className="m-0">
                   At vero eos et accusamus et iusto odio dignissimos ducimus qui
                   blanditiis praesentium voluptatum deleniti atque corrupti quos
@@ -52,10 +100,10 @@ export default function Faq() {
         </div>
 
         <div className="faq-container">
-          <h1>About XYZ</h1>
+          <h1> Connectivity & Network Solutions</h1>
           <div className="card accordion-box ">
             <Accordion activeIndex={0}>
-              <AccordionTab header="Header I">
+              <AccordionTab header="How is FlexNet deployed in an on-premises setup?">
                 <p className="m-0">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -67,7 +115,7 @@ export default function Faq() {
                   mollit anim id est laborum.
                 </p>
               </AccordionTab>
-              <AccordionTab header="Header II">
+              <AccordionTab header="What is the On-Prem Portable solution, and how does it work?">
                 <p className="m-0">
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem aperiam, eaque
@@ -79,7 +127,55 @@ export default function Faq() {
                   eius modi.
                 </p>
               </AccordionTab>
-              <AccordionTab header="Header III">
+              <AccordionTab header="How does FlexNet's cloud-based model work?">
+                <p className="m-0">
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus.
+                </p>
+              </AccordionTab>
+              <AccordionTab header="What kind of devices are compatible with FlexNet’s network?">
+                <p className="m-0">
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus.
+                </p>
+              </AccordionTab>
+              <AccordionTab header="How do I extend FlexNet coverage in remote areas?">
+                <p className="m-0">
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus.
+                </p>
+              </AccordionTab>
+              <AccordionTab header="What equipment is included with each FlexNet deployment">
+                <p className="m-0">
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus.
+                </p>
+              </AccordionTab>
+              <AccordionTab header="What happens if my location doesn’t have public network coverage?">
                 <p className="m-0">
                   At vero eos et accusamus et iusto odio dignissimos ducimus qui
                   blanditiis praesentium voluptatum deleniti atque corrupti quos
@@ -96,10 +192,10 @@ export default function Faq() {
         </div>
 
         <div className="faq-container">
-          <h1>About XYZ</h1>
+          <h1>FlexNet Marketplace</h1>
           <div className="card accordion-box ">
             <Accordion activeIndex={0}>
-              <AccordionTab header="Header I">
+              <AccordionTab header="What is the FlexNet Marketplace?">
                 <p className="m-0">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
                   do eiusmod tempor incididunt ut labore et dolore magna aliqua.
@@ -111,7 +207,7 @@ export default function Faq() {
                   mollit anim id est laborum.
                 </p>
               </AccordionTab>
-              <AccordionTab header="Header II">
+              <AccordionTab header="How do I access applications and devices in the marketplace?">
                 <p className="m-0">
                   Sed ut perspiciatis unde omnis iste natus error sit voluptatem
                   accusantium doloremque laudantium, totam rem aperiam, eaque
@@ -123,7 +219,55 @@ export default function Faq() {
                   eius modi.
                 </p>
               </AccordionTab>
-              <AccordionTab header="Header III">
+              <AccordionTab header="Are the applications tailored for specific industries?">
+                <p className="m-0">
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus.
+                </p>
+              </AccordionTab>
+              <AccordionTab header="Can I customize solutions available in the marketplace?">
+                <p className="m-0">
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus.
+                </p>
+              </AccordionTab>
+              <AccordionTab header="How are marketplace applications integrated with my existing system?">
+                <p className="m-0">
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus.
+                </p>
+              </AccordionTab>
+              <AccordionTab header="How are devices in the marketplace certified for FlexNet?">
+                <p className="m-0">
+                  At vero eos et accusamus et iusto odio dignissimos ducimus qui
+                  blanditiis praesentium voluptatum deleniti atque corrupti quos
+                  dolores et quas molestias excepturi sint occaecati cupiditate
+                  non provident, similique sunt in culpa qui officia deserunt
+                  mollitia animi, id est laborum et dolorum fuga. Et harum
+                  quidem rerum facilis est et expedita distinctio. Nam libero
+                  tempore, cum soluta nobis est eligendi optio cumque nihil
+                  impedit quo minus.
+                </p>
+              </AccordionTab>
+              <AccordionTab header="Can I purchase hardware or software separately from the marketplace?">
                 <p className="m-0">
                   At vero eos et accusamus et iusto odio dignissimos ducimus qui
                   blanditiis praesentium voluptatum deleniti atque corrupti quos
